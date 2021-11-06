3b52f16fc69ac18209bd87f244eefb1f67545634
Marissa Belombre <mbelo@MJBGALAXY>
2021-11-06 12:41:24 -0700

    added code

--- script.js
+++ script.js
@@ -1,24 +1,97 @@
-// Assignment code here
 var lowerCaseChar = [
-  "a",  "b",  "c", "d",  "e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",  "n",  "o",  "p",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",  "z"
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 //uppcase array
 var upperCaseChar = [
-  "A",  "B",  "C",  "D",  "E",  "F",  "G",  "H",  "I",  "J",  "K",  "L",  "M",  "N",  "O",  "P",  "Q",  "R",  "S",  "T",  "U",  "V",  "W",  "X",  "Y",  "Z"
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
 // special char array
 var specialChar = [
-  "@",  "%",  "+",  "\\",  "/",  "'",  "!",  "#",  "$",  "^",  "?",  ":",  ",",  ")",  "(",  "}",  "{",  "]",  "[",  "~",  "-",  "_",  "."
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 
 // Assignment code here
 function generatePasswordInfo() {
-  var getPasswordLength = parseInt(prompt("Choose number of characters for your password."));
-  if(getPasswordLength < 8 ) {
+  var getPasswordLength = parseInt(
+    prompt("Choose number of characters for your password.")
+  );
+  if (getPasswordLength < 8) {
     alert("A minimum number of 8 characters requried.");
     generatePassword();
   }
-  if(getPasswordLength > 128 ) {
+  if (getPasswordLength > 128) {
     alert("A Maximum number of 128 characters requried.");
     generatePassword();
   }
@@ -26,38 +99,43 @@ function generatePasswordInfo() {
     alert("Invalid input. Please insert a number.");
     generatePassword();
   }
-  
-  //Confirm including lowercase letters
-  var includeLowerCase = window.confirm("Click Ok to include lowercase characters");
 
+  //Confirm including lowercase letters
+  var includeLowerCase = window.confirm(
+    "Click Ok to include lowercase characters"
+  );
 
   //Confirm including uppercase letter
-  var includeUpperCase = window.confirm("click Ok to include uppercase characters");
-
+  var includeUpperCase = window.confirm(
+    "click Ok to include uppercase characters"
+  );
 
   //Confirm including numbers
   var includeNumbers = window.confirm("click Ok to include numbers");
 
-
   //Confirm including special characters
   var includespecial = window.confirm("click Ok to include special characters");
 
-
-  if (!includeLowerCase && !includeUpperCase && !includeNumbers && !includespecial) {
+  if (
+    !includeLowerCase &&
+    !includeUpperCase &&
+    !includeNumbers &&
+    !includespecial
+  ) {
     alert("Minimum of 1 character type needed.");
     return;
   }
-  
- //stores password Info in object
-var passwordInfo = {
-  passwordLength: getPasswordLength,
-  includeLowerCase: includeLowerCase,
-  includeUpperCase: includeUpperCase,
-  includeNumbers: includeNumbers,
-  includespecial: includespecial
-};
 
-return passwordInfo; 
+  //stores password Info in object
+  var passwordInfo = {
+    passwordLength: getPasswordLength,
+    includeLowerCase: includeLowerCase,
+    includeUpperCase: includeUpperCase,
+    includeNumbers: includeNumbers,
+    includespecial: includespecial,
+  };
+
+  return passwordInfo;
 }
 
 // Get a random character from an array
@@ -68,12 +146,10 @@ function getRandomChar(arr) {
   return selectedChar;
 }
 
-
 function generatePassword() {
-  
   // passwordInfo object
   var passwordIncludes = generatePasswordInfo();
- 
+
   //store created password
   var createdPassword = [];
 
@@ -81,22 +157,22 @@ function generatePassword() {
   var availableCharacters = [];
 
   // adds array of lower characters to array of available characters
-  if(passwordIncludes.includeLowerCase) {
+  if (passwordIncludes.includeLowerCase) {
     availableCharacters = availableCharacters.concat(lowerCaseChar);
   }
 
   // adds array of upper characters to array of available characters
-  if(passwordIncludes.includeUpperCase) {
+  if (passwordIncludes.includeUpperCase) {
     availableCharacters = availableCharacters.concat(upperCaseChar);
   }
 
   // adds array of numbers to array of available characters
-  if(passwordIncludes.includeNumbers) {
+  if (passwordIncludes.includeNumbers) {
     availableCharacters = availableCharacters.concat(numbersChar);
   }
 
   // adds array of special characters to array of available characters
-  if(passwordIncludes.includespecial) {
+  if (passwordIncludes.includespecial) {
     availableCharacters = availableCharacters.concat(specialChar);
   }
 
@@ -107,12 +183,9 @@ function generatePassword() {
     // add characters to password array
     createdPassword.push(c);
   }
-  
+
   return createdPassword.join("");
-  
-
 }
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
